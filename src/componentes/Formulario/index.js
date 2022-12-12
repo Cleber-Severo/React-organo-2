7737649c7fdd1308deb0f16355b51c61221c3ad4
Cléber Severo <clebersevero1998@gmail.com>
2022-12-11 22:34:25 -0300

    form, creating a new team

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -4,12 +4,14 @@ import CampoTexto from '../CampoTexto'
 import ListaSuspensa from '../ListaSuspensa'
 import './formulario.css'
 
-const Formulario = ({aoCadastrar, times}) => {
+const Formulario = ({aoCadastrar, times, cadastrarTime }) => {
 
     const [nome, setNome] = useState('')
     const [cargo, setCargo] = useState('')
     const [imagem, setImagem] = useState('')
     const [time, setTime] = useState('')
+    const [nomeTime, setNomeTime] = useState('')
+    const [corTime, setCorTime] = useState('')
 
     const aoSubmeter = (evento) => {
         evento.preventDefault()
@@ -25,17 +27,17 @@ const Formulario = ({aoCadastrar, times}) => {
     return (
         <section className="formulario-container">
             <form className="formulario" onSubmit={aoSubmeter}>
-                <h2>Preencha os dados para criar o card do colaborador.</h2>
+                <h2>Preencha os dados para criar um novo time.</h2>
                 <CampoTexto
-                    obrigatorio={true}
+                    obrigatorio
                     label='Nome'
-                    placeholder='Digite seu nome '
+                    placeholder='Digite o nome do time'
                     valor={nome}
                     aoAlterado={valor => setNome(valor)}/>
                 <CampoTexto
-                    obrigatorio={true}
+                    obrigatorio
                     label='Cargo' 
-                    placeholder='Digite seu cargo '
+                    placeholder='Digite ocargo'
                     valor={cargo}
                     aoAlterado={valor => setCargo(valor)}/>
                 <CampoTexto 
@@ -43,11 +45,31 @@ const Formulario = ({aoCadastrar, times}) => {
                     placeholder='Informe o endereço da imagem '
                     aoAlterado={valor => setImagem(valor)}/>
                 <ListaSuspensa 
-                    obrigatorio={true}
+                    obrigatorio
                     label='Times'
                     items={times} 
                     valor={time}
                     aoAlterado={valor => setTime(valor)}/>
+                <Botao texto='Criar card' />
+            </form>
+
+            <form className="formulario" onSubmit={(evento) => {
+                evento.preventDefault()
+                cadastrarTime({nome: nomeTime, cor: corTime})
+            }}>
+                <h2>Preencha os dados para criar o card do colaborador.</h2>
+                <CampoTexto
+                    obrigatorio
+                    label='Nome'
+                    placeholder='Digite seu nome '
+                    valor={nomeTime}
+                    aoAlterado={valor => setNomeTime(valor)}/>
+                <CampoTexto
+                    obrigatorio
+                    label='Cor' 
+                    placeholder='Digite a cor do time'
+                    valor={corTime}
+                    aoAlterado={valor => setCorTime(valor)}/>
                 <Botao texto='Criar card' />
             </form>
         </section>
